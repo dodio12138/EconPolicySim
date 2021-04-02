9524d48db36d42286b13d8a5ce401021fcf48196
dodio12138 <1159715241@qq.com>
2021-04-02 20:33:25 +0800

    1

--- main.py
+++ main.py
@@ -8,12 +8,16 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 
-data = []
-for i in range(50):
-    data.append(np.random.randint(0, 50))
+length = 50
+data = np.zeros((50, 1), dtype=float)
+for i in range(length):
+    data[i] = np.random.randint(0, 50)
 
 pic = plt.figure()
-ax = np.arange(0, 50, 1)
+ax = np.arange(1800, 1800 + length, 1)
+plt.xlabel('年份', fontproperties='SimHei')
+plt.ylabel('GDP')
 plt.plot(ax, data)
 plt.show()
